1f10910f3a4e682c6d525001e2bb2ba932f5beb9
Vineet panwar <vineetpanwar027@gmail.com>
2018-09-01 12:20:30 +0530

    changes to the config

--- server/config/config.js
+++ server/config/config.js
@@ -2,9 +2,18 @@ var env = process.env.NODE_ENV || 'development';
 const dbuser = 'tfg123';
 const dbpassword = 'trade123';
 
-console.log('env is-',process.env.NODE_ENV);
+//console.log('env is-',process.env.NODE_ENV);
+if(env === 'development' || env ==='test'){
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
+}
 
 
+/*
 if(env === 'test'){
   console.log('here');
   process.env.PORT = 3000;
@@ -19,3 +28,4 @@ else{
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 }
+*/
